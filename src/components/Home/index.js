8424f62746f59fef349f25cad49b973eb288fb24
Ricky Giese <richard.giese@icloud.com>
2023-06-23 22:02:25 -0500

    Home app added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,13 @@
-const Home = () => ('hello')
+const Home = () => {
+
+
+    return (
+        <dix className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br /> I'm</h1>
+            </div>
+        </dix>
+    );
+}
 
 export default Home;
