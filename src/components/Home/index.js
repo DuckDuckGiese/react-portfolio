8a433ab091beb2d6dab2a549bd9fe6042b135dd4
Ricky Giese <richard.giese@icloud.com>
2023-06-25 21:26:55 -0500

    name

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,11 @@ const Home = () => {
     return (
         <dix className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm</h1>
+                <h1>Hi, <br /> 
+                I'm Ricky <br /> 
+                Web Developer</h1>
+                <h2>Full Stack Developer</h2>
+                <Link>CONTACT ME</Link>
             </div>
         </dix>
     );
